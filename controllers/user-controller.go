2c77eb9a6fd53198f704dacc496556a433ec8804
yogajulianp <yogajprasutiyo@gmail.com>
2022-10-26 21:42:00 +0700

    update delete data

--- controllers/user-controller.go
+++ controllers/user-controller.go
@@ -73,3 +73,68 @@ func UserControllerGetById(c *fiber.Ctx) error {
 	})
 
 }
+
+func UserControllerUpdate(c *fiber.Ctx) error {
+	userId := c.Params("id")
+
+	var user entity.User
+	err := database.Db.Where("id = ?", userId).First(&user).Error
+	if err != nil {
+		return c.Status(404).JSON(fiber.Map{
+			"message": "user not found",
+		})
+	}
+
+	userRequest := new(entity.User)
+	if err := c.BodyParser(&userRequest); err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"message": "bad request",
+		})
+	}
+
+	//Update User Data  
+	if userRequest.Username != "" {
+		user.Username = userRequest.Username
+	}
+	user.Name = userRequest.Name
+	user.Email = userRequest.Email
+	user.Password = userRequest.Password
+
+	errUpdate := database.Db.Save(&user).Error
+	if errUpdate != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"message" : "server error",
+		})
+	}
+
+	//if succeed
+	return c.JSON(fiber.Map{
+		"message" : "success data has been updated",
+		"data": user,
+	})
+
+}
+
+func UserControllerDelete(c *fiber.Ctx) error {
+	userId := c.Params("id")
+
+	var user entity.User
+	err := database.Db.Debug().Where("id = ?", userId).First(&user).Error
+	if err != nil {
+		return c.Status(404).JSON(fiber.Map{
+			"message": "user not found",
+		})
+	}
+
+	errDelete := database.Db.Debug().Delete(&user).Error
+	if errDelete != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"message" : "server error",
+		})
+	}
+
+		//if succeed
+		return c.JSON(fiber.Map{
+			"message" : "user has been deleted",
+		})
+}
--- route/index-route.go
+++ route/index-route.go
@@ -9,4 +9,6 @@ func RouteInit(r *fiber.App) {
 	r.Get("/user", controllers.UserControllerGetAll)
 	r.Get("/user/:id", controllers.UserControllerGetById)
 	r.Post("/user", controllers.UserControllerCreate)
+	r.Put("/user/:id", controllers.UserControllerUpdate)
+	r.Delete("/user/:id", controllers.UserControllerDelete)
 }
